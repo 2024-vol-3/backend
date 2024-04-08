f4eb5813e5624a17d45cb370f9f789e680d899d3
taku10101 <taku1011ht@gmail.com>
2024-04-08 20:37:27 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # /users
+  resources :users ,only: [:index,:create,:show, :update, :destroy]
+
+  # /groups
+  resources :groups ,only: [:index,:create,:show, :update, :destroy]
+
+  # /groups/[group_id]/issues
+  resources :groups do
+    resources :issues ,only: [:index,:create,:show, :update, :destroy]
+  end
+
+
+
+#/groups/[group_id]/issues/[issue_id]/tag 
+  resources :groups do
+    resources :issues do
+      resources :tags ,only: [:index,:create,:show, :update, :destroy]
+    end
+  end
+
+
+  resources :groups do
+    resources :issues do
+      resources :days ,only: [:index,:show,:create,:update, :destroy]
+    end
+  end
+end
